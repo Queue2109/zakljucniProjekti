5a462e764c3a94b8381aeda05e41b7875cfa3c39
Queue2109 <hj2109@student.uni-lj.si>
2023-05-26 17:18:57 +0200

    Zakljucni projekti

---
+++ Jure/jsk.js
@@ -0,0 +1,10 @@
+function searchbutton() {
+	let input = document.getElementById("kos").value;
+	window.alert("input");
+	if (input == "pekar") {
+		document.getElementById("idk").src = "https://randomicle.com/static/media/sandwich_maker.4d17771a.jpg";
+		window.alert(ćnek);
+	} else if (input == "modelčki za led") {
+	document.getElementById("idk").src = "https://randomicle.com/static/media/sandwich_maker.4d17771a.jpg";
+	}
+}
